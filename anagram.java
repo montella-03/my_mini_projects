6f5f13acdb31f69c867d6af897d1e197978f75de
CHRISTOPHER OIGO <83244477+montella-03@users.noreply.github.com>
2022-07-20 15:58:02 +0300

    Update anagram.java

--- anagram.java
+++ anagram.java
@@ -32,3 +32,47 @@ public class Anagram {
     }
     
 }
+
+// another aproach.
+
+//import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ques {
+
+    //determining whether a strings are anagram or not.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter first string");
+
+        String str1 = in.nextLine();
+
+        System.out.println("enter 2nd string");
+        String str2 = in.nextLine();
+
+        System.out.println("strings are anagram?"+isAnagram(str1, str2));
+
+    }
+
+    private static boolean isAnagram(String str1, String str2) {
+        if(str1.length()!=str2.length())
+            return false;
+
+        int[] countArr = new int[256];
+
+        for (int i = 0; i < str1.length(); i++) {
+            countArr[str1.charAt(i)]++;
+            countArr[str2.charAt(i)]--;
+
+        }
+        for (int j = 0; j < countArr.length; j++) {
+            if (countArr[j] != 0)
+                return false;
+
+        }
+        return true;
+        
+    }
+    
+}
+

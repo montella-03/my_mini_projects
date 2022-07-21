fd0f706b3b3db0377819143c316728ca99caf90a
CHRISTOPHER OIGO <83244477+montella-03@users.noreply.github.com>
2022-07-21 16:12:45 +0300

    Update occOfOddNo.java

--- occOfOddNo.java
+++ occOfOddNo.java
@@ -17,11 +17,30 @@ public class Ques {
         
         for (Map.Entry<Integer, Integer> numberMap : MapCount.entrySet()) {
             if (numberMap.getValue() % 2 != 0) {
-                System.out.println(numberMap.getKey());
+                System.out.println(numberMap.getKey());//ans 5.
+                break;
             }
         }
         
             
+        
+    }
+}
+// using bitwise aproach.
+
+
+
+public class Ques {
+
+    public static void main(String[] args) {
+        int[] name = { 2, 4, 5, 6, 7, 8, 9, 2, 4, };
+
+        int result = 0;
+
+        for (int i = 0; i < name.length; i++) {
+            result = result ^ name[i];
+        }
+        System.out.println(result);//ans 5
         
     }
 }

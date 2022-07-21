49f81a7dc8d43861b1222cabf87c7518dc33d3b9
CHRISTOPHER OIGO <83244477+montella-03@users.noreply.github.com>
2022-07-21 13:20:18 +0300

    Update intersectionArray.java

--- intersectionArray.java
+++ intersectionArray.java
@@ -1,4 +1,4 @@
-
+// o(mn).
 public class Ques {
 
     public static void main(String[] args) {
@@ -14,5 +14,37 @@ public class Ques {
             }
             
         }
+    }
+}
+
+
+// 2 approach o(m+n).
+
+
+public class Ques {
+
+    public static void main(String[] args) {
+        int[] arr1 = { 2, 4, 6, 9, 5 };
+        int[] arr2 = { 1, 3, 4, 7, 9 };
+
+        int i =0;
+        int j = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] == arr2[j]) {
+                System.out.println(arr1[i]);
+                i++;
+                j++;
+            }
+            else if (arr1[i] > arr2[j]) {
+                j++;
+            }
+            else
+                i++;
+
+        }
+        
+            
+        
     }
 }

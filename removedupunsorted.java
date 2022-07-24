20cd720dcb449acb35672de899a6aa016cdbc098
CHRISTOPHER OIGO <83244477+montella-03@users.noreply.github.com>
2022-07-24 13:09:24 +0300

    Update removedupunsorted.java

--- removedupunsorted.java
+++ removedupunsorted.java
@@ -22,3 +22,35 @@ public class Ques {
         }
     }
 }
+// set approach.
+import java.util.HashSet;
+
+import java.util.Set;
+
+public class Ques {
+    public static void main(String[] args) {
+        int[] nums = { 1, 0, 1, 0, 1, 0, 1 };
+        
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            set.add(nums[i]);
+        }
+        
+        set.forEach(e-> System.out.print(e));
+        
+    }
+}
+
+// using hashmap.
+
+Map<Integer, Integer> map = new HashMap<>();
+for(int i=0;i<nums.length;i++){
+    if(map.contains(nums[i]){
+        map.put(nums[i],map.get(nums[i]+1);
+     }
+    else
+        map.put(nums[i],1);
+    }
+    map.forEach((k,v)->sout(k+""));
+                

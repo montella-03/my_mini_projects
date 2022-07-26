728f9d6b3b1109c2a9399a3eac33e68e37b84e4c
CHRISTOPHER OIGO <83244477+montella-03@users.noreply.github.com>
2022-07-26 16:42:40 +0300

    Update singleLinkedlist.java

--- singleLinkedlist.java
+++ singleLinkedlist.java
@@ -9,6 +9,87 @@ public class LL {
 
     }
 
+    public LL.Node find(int value) {// return the node of a value.
+        Node node = head;
+        while (node != null) {
+            if (node.value == value) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+
+    }
+
+    public LL.Node get(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+
+    }
+
+    public int deleteAtIndex(int index) {
+        if (index == 0) {
+            return deleteFirst();
+        }
+        if (index == size - 1) {
+            return deleteLast();
+        }
+        Node previous = get(index - 1);
+        int value = previous.next.value;
+        previous.next = previous.next.next;
+
+        return value;
+        
+    }
+
+    public int deleteLast() {
+        if (size <= 1) {
+            return deleteFirst();
+        }
+        Node secondLast = get(size - 2);
+        int value = tail.value;
+        tail = secondLast;
+        tail.next = null;
+
+        return value;
+
+
+        
+    }
+
+    public int deleteFirst() {
+        int value = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return value;      
+    }
+
+    public void insert(int value, int index) {
+        if (index == 0) {
+            insertAtHead(value);
+            return;
+        }
+        if (index == size) {
+            insertAtTail(value);
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        Node node = new Node(value, temp.next);
+
+        temp.next = node;
+        size++;
+        
+    }
+
     public void insertAtTail(int value) {
         Node node = new Node(value);
         tail.next = node;
@@ -19,7 +100,7 @@ public class LL {
         size++;
     }
 
-    public void display() {
+     public void display() {
         Node temp = head;
         while (temp != null) {
             System.out.print(temp.value + "--->");
@@ -55,6 +136,7 @@ public class LL {
         }
     }
 }
+
 //
 
 main class.
